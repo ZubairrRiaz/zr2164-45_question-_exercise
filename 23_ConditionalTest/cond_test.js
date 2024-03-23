37a789ef00bdc86eee788e4efdb226e4c27a4b7f
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:16:59 -0700

    Add files via upload

---
+++ 23_ConditionalTest/cond_test.js
@@ -0,0 +1,32 @@
+"use strict";
+let car = 'subaru';
+//Test No 1
+console.log("Is car == 'subaru?' I predict True.");
+console.log(car == 'subaru');
+//Test No 2
+console.log("Is car === 'subaru?' I predict True.");
+console.log(car === 'subaru');
+//Test No 3
+console.log("Is car != 'Honda?' I predict True.");
+console.log(car != 'Honda');
+//Test No 4
+console.log("Is car !== 'Honda'I predict True.");
+console.log(car !== 'Honda');
+//Test No 5
+console.log("Is car !== 'SUBARU'I predict True");
+console.log(car.toUpperCase() == 'SUBARU');
+//Test No 6
+console.log("Is car == 'SUBARU'I predict false");
+console.log(car == 'SUBARU');
+//Test No 7
+console.log("Is car === 'SUBARU'I predict false");
+console.log(car === 'SUBARU');
+//Test No 8
+console.log("Is car == 'BMW'I predict false");
+console.log(car == 'BMW');
+//Test No 9
+console.log("Is car === 'Bike'I predict false");
+console.log(car === 'Bike');
+//Test No 10
+console.log("Is car !== 'cycle'I predict false");
+console.log(car.toLowerCase() === 'SUBARU');
