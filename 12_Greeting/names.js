6da82c0ebf9cf5d4288292622f2201aeb8611552
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 08:52:22 -0700

    Add files via upload

---
+++ 12_Greeting/names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['zubair', 'hasnain', 'waqas', 'umer'];
+const message = 'HI How Are You: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12_Greeting/names.ts
@@ -0,0 +1,7 @@
+let names: string[] = ['zubair','hasnain','waqas','umer'];
+
+const message: string = 'HI How Are You: ';
+
+for( let i=0; i<names.length; i++){
+    console.log(message + names[i]);
+}
