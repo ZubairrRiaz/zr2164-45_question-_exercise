bc4a02539c3a62bc6e2a256e36ddee9da918b109
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 08:51:03 -0700

    Add files via upload

---
+++ 11_Names/names.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['zubair', 'hasnain', 'waqas', 'umer'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11_Names/names.ts
@@ -0,0 +1,5 @@
+let names: string[] = ['zubair','hasnain','waqas','umer'];
+
+for( let i=0; i<names.length; i++){
+    console.log(names[i]);
+}
