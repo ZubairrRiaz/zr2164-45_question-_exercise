a793c2dffe1e30e44f74e0738f309a1f78ca6cf0
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 08:48:19 -0700

    Add files via upload

---
+++ 9_Favourite_number/favorite_number.js
@@ -0,0 +1,4 @@
+"use strict";
+const favoriteNumber = 7;
+const message = 'Here is your favorite number ' + favoriteNumber;
+console.log(message);
---
+++ 9_Favourite_number/favorite_number.ts
@@ -0,0 +1,5 @@
+const favoriteNumber: number = 7;
+const message: string = 'Here is your favorite number ' + favoriteNumber;
+console.log(message);
+
+
