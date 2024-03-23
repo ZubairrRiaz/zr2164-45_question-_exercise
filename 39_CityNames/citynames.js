63786b992309b361709329450ed943628acfdd25
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 10:11:13 -0700

    Add files via upload

---
+++ 39_CityNames/citynames.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_country(city, country) {
+    return city + ',' + country;
+}
+console.log(city_country(`"Delhi"`, `"India"`));
+console.log(city_country(`"New York"`, `"America"`));
+console.log(city_country(`"Bhejing"`, `"China"`));
---
+++ 39_CityNames/citynames.ts
@@ -0,0 +1,11 @@
+
+
+function city_country(city:string, country:string){
+    return city+','+country
+}
+
+console.log(city_country(`"Delhi"`,`"India"`));
+
+console.log(city_country(`"New York"`,`"America"`));
+
+console.log(city_country(`"Bhejing"`,`"China"`));
