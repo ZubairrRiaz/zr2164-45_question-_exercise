72a2ef5a122ac66083038c979bcb81e7bf7d9559
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 08:49:41 -0700

    Add files via upload

---
+++ 10_Adding_comment/add.js
@@ -0,0 +1,5 @@
+"use strict";
+//PROGRAM WRITTEN BY 'ZUBAIR'
+//CURRENT DATE; 14/2/2024
+//THIS PROGRAM WILL PRINT 'HELLO WORLD'
+console.log('Hello world');
---
+++ 10_Adding_comment/add.ts
@@ -0,0 +1,9 @@
+
+
+//PROGRAM WRITTEN BY 'ZUBAIR'
+
+//CURRENT DATE; 14/2/2024
+
+//THIS PROGRAM WILL PRINT 'HELLO WORLD'
+
+console.log('Hello world');
