9c0cece7c1d680027588c0cecb561bd953e82f30
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 08:39:39 -0700

    Add files via upload

---
+++ 4_Famous_quote/famous_Quote.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log('Nelson Mandela once said "A Winner is a dreamer who never gives up"');
---
+++ 4_Famous_quote/famous_Quote.ts
@@ -0,0 +1,1 @@
+console.log('Nelson Mandela once said "A Winner is a dreamer who never gives up"');
