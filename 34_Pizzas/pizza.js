c967603e94343b6b9c6ae6fe01b9847eab1771fc
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 10:05:51 -0700

    Add files via upload

---
+++ 34_Pizzas/pizza.js
@@ -0,0 +1,6 @@
+"use strict";
+let pizza = ['vegetable', 'BBQ chicken', 'pepperoni'];
+for (let pizzas of pizza) {
+    console.log(`i love ${pizzas} pizza its very tatsy;`);
+}
+console.log('i Really like pizza!');
---
+++ 34_Pizzas/pizza.ts
@@ -0,0 +1,9 @@
+
+let pizza:string[] = ['vegetable','BBQ chicken','pepperoni'];
+
+for(let pizzas of pizza ){
+    console.log(`i love ${pizzas} pizza its very tatsy;`);
+    
+}
+
+console.log('i Really like pizza!');
