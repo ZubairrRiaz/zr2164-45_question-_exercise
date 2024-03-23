c6365beeebe36751f300fb42d0ea64c4cf41c318
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 10:13:37 -0700

    Add files via upload

---
+++ 41_Magicians/magician.js
@@ -0,0 +1,8 @@
+"use strict";
+let magician = ['ben10', 'oggy', 'bheem', 'doraemon'];
+function show_magician(magician) {
+    magician.forEach(element => {
+        console.log(element);
+    });
+}
+show_magician(magician);
---
+++ 41_Magicians/magician.ts
@@ -0,0 +1,13 @@
+
+
+let magician: string[] = ['ben 10','oggy','bheem','doraemon'];
+
+function show_magician (magician:string[]){
+
+    magician.forEach(element => {
+        
+        console.log(element);
+        
+    });
+}
+show_magician(magician);
