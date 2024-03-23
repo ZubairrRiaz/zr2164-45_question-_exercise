f8e3c5df19d7b43638787ba85085833eda42602c
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:07:23 -0700

    Add files via upload

---
+++ 19_DinnerGuest/guest_list.js
@@ -0,0 +1,36 @@
+"use strict";
+let GuestNames = ['zubair', 'hasnain', 'waqas', 'umer'];
+//for(let i=0; i<GuestNames.length; i++){
+//console.log('\nAssalamuAlaikum. ' + GuestNames[i] + '\n\n it is our pleasure to invite you in our party.\n\n Thank you!\n')
+//};
+//guest replace kar rahe hain
+let absentGuest = 'Zubair';
+let comingGuest = 'Lahori';
+GuestNames[0] = comingGuest;
+for (let i = 0; i < GuestNames.length; i++) {
+    console.log('\nAssalamuAlaikum. ' + GuestNames[i] + '\n\n it is our pleasure to invite you in our party.\n\n Thank you!\n');
+}
+;
+//console.log(`Mr ${absentGuest} is not coming.`);
+// 3 aur guest ko invite kar rahe hain
+//console.log('Good news! we found big table so we are inviting 3 more guest.');
+GuestNames.unshift('Hamza');
+GuestNames.splice(2, 0, 'Riaz');
+GuestNames.push('bilal');
+//for(let i=0; i<GuestNames.length; i++){
+//console.log('\nAssalamuAlaikum. ' + GuestNames[i] + '\n\n it is our pleasure to invite you in our party.\n\n Thank you!\n')
+//};
+// 4 guest ko nikal rahe hum
+//console.log('Sorry we are inviting only two people because the table did not arrive.');
+while (GuestNames.length > 2) {
+    let removeGuest = GuestNames.pop();
+    // console.log(`Dear ${removeGuest}.we are sorry you are not invited.`);
+}
+// 2 guest ko invite kar rahe hain
+// for (let i=0; i<GuestNames.length; i++){
+//console.log('\nAssalamuAlaikum. ' + GuestNames[i] + '\n\n You are still invited.\n\n Thank you!\n')
+//};
+// sab ko nikal diya
+GuestNames.splice(0, 2);
+console.log(GuestNames);
+console.log(`Total number of guest are ${GuestNames.length}`);
