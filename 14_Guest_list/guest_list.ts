bfd478209969e90452271405a869e8ea804da552
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 08:55:03 -0700

    Add files via upload

---
+++ 14_Guest_list/guest_list.ts
@@ -0,0 +1,6 @@
+let GuestNames: string[] = ['zubair','hasnain','waqas','umer'];
+
+for(let i=0; i<GuestNames.length; i++){
+
+console.log('\nAssalamuAlaikum. ' + GuestNames[i] + '\n\n it is our pleasure to invite you in our party.\n\n Thank you!\n')
+};
