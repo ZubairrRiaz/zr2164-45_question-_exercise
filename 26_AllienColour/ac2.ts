f652b5067922b313683b48dd8028ffaf6ce3d857
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:42:59 -0700

    Add files via upload

---
+++ 26_AllienColour/ac2.ts
@@ -0,0 +1,20 @@
+
+let Allien_colour2 = "Green";
+
+if(Allien_colour2 === "Green"){
+
+console.log("The player just earn 5 points for shooting the allien!");
+}
+else{
+   console.log("player just earn 10 points!");
+}
+
+Allien_colour2 = "yellow";
+
+if(Allien_colour2 === "Green"){
+
+    console.log("The player just earn 5 points for shooting the allien!");
+}
+else{
+       console.log("player just earn 10 points!");
+}
