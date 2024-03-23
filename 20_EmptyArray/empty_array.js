6d66d58484f221d00f1d5cd6e3e0796634384da6
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:11:02 -0700

    Add files via upload

---
+++ 20_EmptyArray/empty_array.js
@@ -0,0 +1,7 @@
+"use strict";
+let items = ['Pakistan', 'Karachi', 'Punjabi'];
+let items2 = [];
+items2.push('france');
+items2.push('iran');
+items2.push('Uae');
+console.log(items2);
---
+++ 20_EmptyArray/empty_array.ts
@@ -0,0 +1,12 @@
+
+
+
+let items: string[] = ['Pakistan','Karachi','Punjabi'];
+
+let items2: string[] = [];
+
+items2.push('france');
+items2.push('iran');
+items2.push('Uae');
+
+console.log(items2);
