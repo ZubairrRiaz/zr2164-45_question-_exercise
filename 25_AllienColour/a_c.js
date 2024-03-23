33e48c08422243d80e1dd8b0f394846f044d6b54
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:36:44 -0700

    Add files via upload

---
+++ 25_AllienColour/a_c.js
@@ -0,0 +1,9 @@
+"use strict";
+let alien_colour = "blue";
+if (alien_colour === "blue") {
+    console.log("player just earn 5 points !");
+}
+let Alien_colour = "black";
+if (alien_colour === "red") {
+    console.log('No output');
+}
