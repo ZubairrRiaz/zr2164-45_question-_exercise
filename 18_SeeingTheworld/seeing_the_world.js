b0ebd4bdd159f79963690aa9c4bf21595e0c0987
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:02:54 -0700

    Add files via upload

---
+++ 18_SeeingTheworld/seeing_the_world.js
@@ -0,0 +1,11 @@
+"use strict";
+let places = ['makkah', 'madina', 'russia', 'usa', 'thailand'];
+console.log('Original ' + places);
+console.log('Copy ' + [...places].sort());
+console.log('Original ' + places);
+console.log('Copy ' + [...places].sort().reverse());
+console.log('Original ' + places);
+console.log('Original ' + places.reverse());
+console.log('Original ' + places.reverse());
+console.log('Original ' + places.sort());
+console.log('Original ' + places.sort().reverse());
---
+++ 18_SeeingTheworld/seeing_the_world.ts
@@ -0,0 +1,22 @@
+
+
+let places: string[] = ['makkah','madina','russia','usa','thailand'];
+
+console.log('Original ' + places);
+
+console.log('Copy ' + [...places].sort());
+
+console.log('Original ' + places);
+
+console.log('Copy ' + [...places].sort().reverse());
+
+console.log('Original ' + places);
+
+console.log('Original ' + places.reverse());
+
+console.log('Original ' + places.reverse());
+
+console.log('Original ' + places.sort());
+
+console.log('Original ' + places.sort().reverse());
+
