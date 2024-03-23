f80c1741294b3dbe898e8a74b098c957a3ed9a7f
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:51:26 -0700

    Add files via upload

---
+++ 27_AllienColurs3/ac3.js
@@ -0,0 +1,38 @@
+var allienColour = "green";
+if (allienColour === "green") {
+    console.log('The player just earn 5 points!');
+}
+else if (allienColour === "yellow") {
+    console.log('Player just earn 10 points!');
+}
+else {
+    console.log('Not match!');
+}
+;
+/***********************************************************************/
+allienColour = "yellow";
+if (allienColour === "green") {
+    console.log('The player just earn 5 points!');
+}
+else if (allienColour === "yellow") {
+    console.log('Player just earn 10 points!');
+}
+else {
+    console.log('Not match!');
+}
+;
+/***********************************************************************/
+allienColour = 'red';
+if (allienColour === "green") {
+    console.log('The player just earn 5 points!');
+}
+if (allienColour == 'red') {
+    console.log('Player just earn 15 points!');
+}
+else if (allienColour === "yellow") {
+    console.log('Player just earn 10 points!');
+}
+else {
+    console.log('Not match!');
+}
+;
---
+++ 27_AllienColurs3/ac3.ts
@@ -0,0 +1,59 @@
+
+let allienColour: string = "green";
+
+if(allienColour === "green"){
+
+    console.log('Versian 1. The player just earn 5 points!');
+    
+}
+else if(allienColour === "yellow"){
+
+    console.log('Player just earn 10 points!');
+    
+}
+else{
+    console.log('Not match!');
+
+};
+
+/***********************************************************************/ 
+
+allienColour = "yellow";
+
+if(allienColour === "green"){
+
+    console.log('The player just earn 5 points!');
+    
+}
+else if(allienColour === "yellow"){
+
+    console.log('Versian 2. Player just earn 10 points!');
+    
+}
+else{
+    console.log('Not match!');
+    
+};
+
+/***********************************************************************/ 
+
+allienColour = 'red';
+
+if(allienColour === "green"){
+
+    console.log('The player just earn 5 points!');
+    
+}
+if(allienColour == 'red'){
+    console.log('Versian 3. Player just earn 15 points!');
+    
+}
+else if(allienColour === "yellow"){
+
+    console.log('Player just earn 10 points!');
+    
+}
+else{
+    console.log('Not match!');
+    
+};
