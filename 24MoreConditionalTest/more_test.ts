6ecfd5e922308e40a0d6c4c6cf0927d3343225f1
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:31:37 -0700

    Add files via upload

---
+++ 24MoreConditionalTest/more_test.ts
@@ -0,0 +1,52 @@
+
+
+//Eqaulity or ineqaulity test
+
+console.log("Eqaulity test with string: " , "Apple"==="Apple");
+
+//Eqaulity or ineqaulity test
+
+console.log("Eqaulity test with string: " ,("Apple" as string)==="APPLE");
+
+//Test using lower case function
+
+console.log("Lower case test: ","APPLE".toLowerCase()=="apple");
+
+//Test using lower case function
+
+console.log("Lower case test: ","APPLE".toLowerCase()=="APPLE");
+
+//Numericals Eqaulity or ineqaulity test
+
+console.log("Numerical test: ", 26===26);
+
+//Greater than and less than  test
+
+console.log("greater than test: ", 26 !> 26);
+
+//Greater than and less than  test
+
+console.log("greater than test: ", 50 > 25);
+
+//Greater than and less than  test
+
+console.log("less than test: ", 50 <= 100);
+
+// && and || operator tets
+
+console.log("&& operator test: ", 5===5 && 30 > 20 );
+
+// && and || operator tets
+
+console.log("|| operator test: ", 5===5 || 20 > 30 );
+
+//test wether an item is a array
+
+const fruits: string[] =['banana','watermelon','strawbery','Melon']
+
+console.log("Strawbery in array Test: ", fruits.includes('strawbery'));
+
+//test wether an item is a array
+
+
+console.log("Apple is not in array Test: ", fruits.includes('Aplle'));
