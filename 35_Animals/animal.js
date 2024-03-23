1010bf5b0c89d88fc15e3a22aefe08c9fcd5d09b
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 10:07:12 -0700

    Add files via upload

---
+++ 35_Animals/animal.js
@@ -0,0 +1,15 @@
+"use strict";
+let animal = ['cat', 'dog', 'chicks'];
+for (let animals of animal) {
+    if (animals == 'cat') {
+        console.log(`\ni like ${animals} bcz its smart animal!\n`);
+    }
+    else if (animals == 'dog') {
+        console.log(`\ni really love ${animals} bcz its loyal animal!\n`);
+    }
+    else {
+        console.log(`\ni really love ${animals} its very cute!\n`);
+    }
+}
+;
+console.log('\nAny of these animals would make a great pet;');
