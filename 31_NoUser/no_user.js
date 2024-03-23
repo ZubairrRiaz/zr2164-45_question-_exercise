3a85993ffe61f5745a833a1256115187da985bf1
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:59:49 -0700

    Add files via upload

---
+++ 31_NoUser/no_user.js
@@ -0,0 +1,9 @@
+"use strict";
+let userNames = ['admin', 'zubair', 'hasnain', 'waqas', 'umer'];
+if (userNames.length == 0) {
+    console.log('We need to find some user;');
+}
+else {
+    userNames = [];
+    console.log('All user names have been remove. ' + userNames.length);
+}
---
+++ 31_NoUser/no_user.ts
@@ -0,0 +1,12 @@
+
+let userNames: string[] = ['admin','zubair','hasnain','waqas','umer'];
+
+if(userNames.length == 0){
+    console.log('We need to find some user;');
+
+}else {
+    userNames = [];
+    console.log('All user names have been remove. '+ userNames.length);
+    
+}
+    
