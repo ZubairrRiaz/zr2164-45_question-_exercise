48a5e460ea7150779d975645e70be5b8bd5888d4
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 09:26:04 -0700

    Delete 21_TsObject/22_InternationalError directory

--- 21_TsObject/22_InternationalError/int_error.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let newArray = ['hasnain', 'zubair', 'waqas'];
-console.log(newArray[3]);
-console.log(newArray);
--- 21_TsObject/22_InternationalError/int_error.ts
+++
@@ -1,7 +0,0 @@
-
-
-let newArray: string[] =['hasnain','zubair','waqas'];
-
-console.log(newArray[3]);
-
-console.log(newArray);
