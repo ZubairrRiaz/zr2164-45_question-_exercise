b17274a21fa8a67c8bcb1f7cabf19f9e877e6a6f
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 10:08:22 -0700

    Add files via upload

---
+++ 36_Tshirt/tshirt.js
@@ -0,0 +1,5 @@
+"use strict";
+function make_shirt(size, print) {
+    console.log(`\nThis is the best shirt with ${size} size and ${print} brand;`);
+}
+make_shirt('Large', 'Gucci');
---
+++ 36_Tshirt/tshirt.ts
@@ -0,0 +1,8 @@
+
+
+function make_shirt(size:string, print:string){
+
+    console.log(`\nThis is the best shirt with ${size} size and ${print} brand;`);
+    
+}
+make_shirt('Large','Gucci');
