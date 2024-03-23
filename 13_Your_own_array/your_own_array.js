779acb852d5ead6a21e78a87fd4cb0f37aa1d214
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 08:53:57 -0700

    Add files via upload

---
+++ 13_Your_own_array/your_own_array.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ['bike', 'car', 'train', 'aeroplane'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('i would like to drive a ' + transportation[i]);
+}
---
+++ 13_Your_own_array/your_own_array.ts
@@ -0,0 +1,6 @@
+
+let transportation: string[] = ['bike','car','train','aeroplane'];
+for(let i=0; i<transportation.length; i++){
+
+    console.log('i would like to drive a ' + transportation[i]);
+}
