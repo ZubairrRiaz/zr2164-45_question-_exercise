2be1705bfb1b01f95409c37927479fcd715b0d41
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 10:10:18 -0700

    Add files via upload

---
+++ 38_Cities/cities.ts
@@ -0,0 +1,12 @@
+
+
+function describe_city(city:string, country:string= 'default country'){
+
+    console.log(`${city} is in ${country};`);
+    
+}
+describe_city('karachi','Pakistan');
+
+describe_city('Moscow','Russia')
+
+describe_city('North korea')
