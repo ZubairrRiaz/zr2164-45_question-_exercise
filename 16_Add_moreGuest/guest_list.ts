9e401418c62153f9a647ce1a8cad0493757d3236
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 08:59:06 -0700

    Add files via upload

---
+++ 16_Add_moreGuest/guest_list.ts
@@ -0,0 +1,38 @@
+let GuestNames: string[] = ['zubair','hasnain','waqas','umer'];
+
+//for(let i=0; i<GuestNames.length; i++){
+
+//console.log('\nAssalamuAlaikum. ' + GuestNames[i] + '\n\n it is our pleasure to invite you in our party.\n\n Thank you!\n')
+//};
+
+let absentGuest: string = 'Zubair';
+
+let comingGuest: string = 'Lahori';
+
+GuestNames[0] = comingGuest;
+
+for(let i=0; i<GuestNames.length; i++){
+
+    console.log('\nAssalamuAlaikum. ' + GuestNames[i] + '\n\n it is our pleasure to invite you in our party.\n\n Thank you!\n')
+    };
+
+
+
+console.log(`Mr ${absentGuest} is not coming.`);
+
+console.log('Good news! we found big table so we are inviting 3 more guest.');
+
+GuestNames.unshift('Hamza');
+
+GuestNames.splice(2, 0, 'Riaz');
+
+GuestNames.push('bilal');
+for(let i=0; i<GuestNames.length; i++){
+
+    console.log('\nAssalamuAlaikum. ' + GuestNames[i] + '\n\n it is our pleasure to invite you in our party.\n\n Thank you!\n')
+    };
+
+
+
+
+
