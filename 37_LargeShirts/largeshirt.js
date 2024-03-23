69917c9347f87328223fd315fd66dbf54451ae67
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 10:09:24 -0700

    Add files via upload

---
+++ 37_LargeShirts/largeshirt.js
@@ -0,0 +1,7 @@
+"use strict";
+function make_shirt(size = 'Large', message = 'Puma') {
+    console.log(`\nThis is the best shirt with '${size}' size and '${message}' brand;\n`);
+}
+make_shirt();
+make_shirt('Medium', 'Prada');
+make_shirt('small', 'Nike');
---
+++ 37_LargeShirts/largeshirt.ts
@@ -0,0 +1,11 @@
+
+function make_shirt(size:string= 'Large', message:string= 'Puma'){
+
+    console.log(`\nThis is the best shirt with '${size}' size and '${message}' brand;\n`);
+    
+}
+make_shirt();
+
+make_shirt('Medium','Prada');
+
+make_shirt('small','Nike')
