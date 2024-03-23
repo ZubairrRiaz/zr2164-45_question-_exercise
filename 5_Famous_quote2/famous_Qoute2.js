bdece1cedcdc9f4f5c4e3236c16511175c2346a8
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-03-23 08:42:34 -0700

    Add files via upload

---
+++ 5_Famous_quote2/famous_Qoute2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = 'Nelson Mandela';
+let message = ' Once Said "A Winner is a dreamer who never gives up"';
+console.log(`${famous_person} ${message}`);
---
+++ 5_Famous_quote2/famous_Qoute2.ts
@@ -0,0 +1,3 @@
+let famous_person: string = 'Nelson Mandela';
+let message: string = ' Once Said "A Winner is a dreamer who never gives up"';
+console.log(`${famous_person} ${message}`); 
